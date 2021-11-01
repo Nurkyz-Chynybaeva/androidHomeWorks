b03ec8964ba6fb4ef3fe2140d74d2865aa10d4a8
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-11-01 17:49:52 +0600

    11th lesson

--- app/src/main/java/com/example/homeworks/MainActivity.kt
+++ app/src/main/java/com/example/homeworks/MainActivity.kt
@@ -1,25 +1,38 @@
 package com.example.homeworks
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import androidx.constraintlayout.widget.ConstraintSet
 
 class MainActivity : AppCompatActivity(){                                  //..., View.OnClickListener
 
        private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.card_view)
+        setContentView(R.layout.hw_20_10_21)
 
-//        TextView = findViewById<TextView>(R.id.textView)
-//        val btn1 = findViewById<AppCompatButton>(R.id.btn1)
-//        val btn2 = findViewById<AppCompatButton>(R.id.btn2)
-//        val btn3 = findViewById<AppCompatButton>(R.id.btn3)
-//
+        textView = findViewById(R.id.textView)
+
+        val btn1 = findViewById<AppCompatButton>(R.id.btn1)
+        val btn2 = findViewById<AppCompatButton>(R.id.btn2)
+        val btn3 = findViewById<AppCompatButton>(R.id.btn3)
+
+        btn1.setOnClickListener{
+            textView.text = "first click 1"
+        }
+        btn2.setOnClickListener{
+            textView.text = "second click 2"
+        }
+        btn3.setOnClickListener{
+            textView.text = "third click 3"
+        }
 //
 //    btn1.setOnClickListener{
 //        TextView.text = "first click"
