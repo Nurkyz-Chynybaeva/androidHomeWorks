8ccc99fe757d2ba1de60f1f8f307315caed2e172
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-11-25 10:49:07 +0600

    added new changes

--- app/src/main/java/com/example/homeworks/MainActivity.kt
+++ app/src/main/java/com/example/homeworks/MainActivity.kt
@@ -7,7 +7,9 @@ import android.text.Layout
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import androidx.constraintlayout.widget.ConstraintSet
 
 class MainActivity : AppCompatActivity(){                                  //..., View.OnClickListener
 
