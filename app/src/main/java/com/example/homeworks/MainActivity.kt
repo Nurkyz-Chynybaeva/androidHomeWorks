bf63388454e86c7be297dcea1ed3d51dc7bead19
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-11-27 17:05:18 +0600

    calculator

--- app/src/main/java/com/example/homeworks/MainActivity.kt
+++ app/src/main/java/com/example/homeworks/MainActivity.kt
@@ -2,30 +2,81 @@ package com.example.homeworks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import androidx.appcompat.widget.AppCompatButton
+import android.util.Log
+import com.example.homeworks.databinding.ActivityMainBinding
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var textView: TextView
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            //numbers
+            one.setOnClickListener { appendonExpresstion("1", true) }
+            two.setOnClickListener { appendonExpresstion("2", true) }
+            three.setOnClickListener { appendonExpresstion("3", true) }
+            four.setOnClickListener { appendonExpresstion("4", true) }
+            five.setOnClickListener { appendonExpresstion("5", true) }
+            six.setOnClickListener { appendonExpresstion("6", true) }
+            seven.setOnClickListener { appendonExpresstion("7", true) }
+            eight.setOnClickListener { appendonExpresstion("8", true) }
+            nine.setOnClickListener { appendonExpresstion("9", true) }
+            zero.setOnClickListener { appendonExpresstion("0", true) }
+            //operators
+            plus.setOnClickListener { appendonExpresstion("+", false) }
+            minus.setOnClickListener { appendonExpresstion("-", false) }
+            multiplication.setOnClickListener { appendonExpresstion("*", false) }
+            division.setOnClickListener { appendonExpresstion("/", false) }
+            firstBracket.setOnClickListener { appendonExpresstion("(", false) }
+            secondBracket.setOnClickListener { appendonExpresstion(")", false) }
+            point.setOnClickListener { appendonExpresstion(".", false) }
+            clean.setOnClickListener {
+                expression.text = ""
+                result.text = ""
+            }
+            back.setOnClickListener {
+                val string = expression.text.toString()
+                if (string.isNotEmpty()) {
+                    expression.text = string.substring(0, string.length - 1)
+                }
+                result.text = ""
+            }
+            equals.setOnClickListener {
+                try {
+                    val expression = ExpressionBuilder(binding.expression.text.toString()).build()
+                    val result = expression.evaluate()
+                    val longResult = result.toLong()
+                    if (result == longResult.toDouble())
+                        binding.result.text = longResult.toString()
+                    else
+                        binding.result.text = result.toString()
 
-        textView = findViewById(R.id.textView)
-
-        val btn1 = findViewById<AppCompatButton>(R.id.btn1)
-        val btn2 = findViewById<AppCompatButton>(R.id.btn2)
-        val btn3 = findViewById<AppCompatButton>(R.id.btn3)
-
-        btn1.setOnClickListener {
-            textView.text = "first click 1"
+                } catch (e: Exception) {
+                    Log.d("Exeption", "msg:" + e.message)
+                    binding.result.text = "error!"
+                } catch (e: ArithmeticException) {
+                    Log.e("Exeption :", "msg:" + e.message)
+                    binding.result.text = "error!"
+                }
+            }
         }
-        btn2.setOnClickListener {
-            textView.text = "second click 2"
-        }
-        btn3.setOnClickListener {
-            textView.text = "third click 3"
+    }
+
+    fun appendonExpresstion(string: String, canClear: Boolean) {
+        binding.apply {
+            if (result.text.isNotEmpty()) {
+                expression.text = ""
+            }
+            if (canClear) {
+                result.text = ""
+                expression.append(string)
+            } else {
+                expression.append(binding.result.text)
+                expression.append(string)
+                result.text = ""
+            }
         }
     }
 }
