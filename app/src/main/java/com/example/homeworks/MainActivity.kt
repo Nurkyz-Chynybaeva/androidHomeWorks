1840200c6c71148e16b6acb9043d69925e540c1d
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-11-27 22:03:23 +0600

    task with email

--- app/src/main/java/com/example/homeworks/MainActivity.kt
+++ app/src/main/java/com/example/homeworks/MainActivity.kt
@@ -1,31 +1,38 @@
 package com.example.homeworks
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import androidx.appcompat.widget.AppCompatButton
+import android.widget.Toast
+import com.example.homeworks.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var textView: TextView
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        textView = findViewById(R.id.textView)
-
-        val btn1 = findViewById<AppCompatButton>(R.id.btn1)
-        val btn2 = findViewById<AppCompatButton>(R.id.btn2)
-        val btn3 = findViewById<AppCompatButton>(R.id.btn3)
-
-        btn1.setOnClickListener {
-            textView.text = "first click 1"
-        }
-        btn2.setOnClickListener {
-            textView.text = "second click 2"
-        }
-        btn3.setOnClickListener {
-            textView.text = "third click 3"
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btn.setOnClickListener {
+            val email = binding.emailC.text.toString()
+            val subject = binding.subjectC.text.toString()
+            val message = binding.messageC.text.toString()
+            val address = email.split(",".toRegex()).toTypedArray()
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("to:")
+                putExtra(Intent.EXTRA_EMAIL, address)
+                putExtra(Intent.EXTRA_SUBJECT, subject)
+                putExtra(Intent.EXTRA_TEXT, message)
+            }
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    "You do not have any mail applications preinstalled",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 }
